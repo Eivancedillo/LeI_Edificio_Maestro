c41edf628d16fd7dc01222e242a3caebdb41947f
Eivan <eivan.ecedillo@gmail.com>
2026-04-27 19:24:28 -0600

    Modulo pantalla planta baja implementado

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,22 @@
 #include <Arduino.h>
 #include <esp_now.h>
 #include <WiFi.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
 
-// --- PINES DEL MAESTRO ---
+// --- PINES DE LA PANTALLA TFT ---
+#define TFT_CS 15
+#define TFT_DC 2
+#define TFT_RST 4
+
+// --- PINES DEL MAESTRO (Actualizados) ---
 #define TRIG_PIN 5
-#define ECHO_PIN 18
-#define IR_PIN 4
+#define ECHO_PIN 12 // ¡Nuevo hogar!
+#define IR_PIN 19   // ¡Nuevo hogar!
 #define LDR_PIN 34
-#define BOTON_PIN 23
-#define BUZZER_PIN 25 // Pin para el transistor del Buzzer
+#define BOTON_PIN 13 // ¡Nuevo hogar!
+#define BUZZER_PIN 25
 
 // --- AJUSTES EDITABLES ---
 const int umbralLuz = 1000;
@@ -28,7 +36,11 @@ unsigned long ultimaVezDetectadoPasillo = 0;
 unsigned long ultimaVezDetectadoEntrada = 0;
 bool fiestaActiva = false;
 
+// MAC del Esclavo
 uint8_t slaveAddress[] = {0x08, 0xD1, 0xF9, 0xD2, 0x22, 0xF4};
+
+// Objeto de la pantalla
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
 
 typedef struct struct_message
 {
@@ -43,6 +55,28 @@ esp_now_peer_info_t peerInfo;
 void setup()
 {
     Serial.begin(115200);
+
+    // --- SETUP DE LA PANTALLA ---
+    tft.begin();
+    tft.setRotation(1);
+    tft.fillScreen(ILI9341_BLACK);
+
+    tft.setCursor(10, 30);
+    tft.setTextColor(ILI9341_GREEN);
+    tft.setTextSize(3);
+    tft.println("Edificio inteligente");
+
+    tft.setCursor(10, 65);
+    tft.setTextColor(ILI9341_CYAN);
+    tft.setTextSize(1);
+    tft.println("Lenguajes e Interdaces");
+
+    tft.setCursor(10, 100);
+    tft.setTextColor(ILI9341_WHITE);
+    tft.setTextSize(2);
+    tft.println("Sistema en Linea");
+    // ----------------------------
+
     pinMode(TRIG_PIN, OUTPUT);
     pinMode(ECHO_PIN, INPUT);
     pinMode(IR_PIN, INPUT);
@@ -62,7 +96,7 @@ void loop()
 {
     unsigned long tiempoActual = millis();
 
-    // 1. LÓGICA DE SENSORES (Pasillo y Entrada)
+    // 1. PASILLO
     digitalWrite(TRIG_PIN, LOW);
     delayMicroseconds(2);
     digitalWrite(TRIG_PIN, HIGH);
@@ -80,6 +114,7 @@ void loop()
         datosParaEnviar.presenciaPasillo = (tiempoActual - ultimaVezDetectadoPasillo < tiempoEsperaLeds);
     }
 
+    // 2. ENTRADA
     if (digitalRead(IR_PIN) == LOW)
     {
         ultimaVezDetectadoEntrada = tiempoActual;
@@ -90,7 +125,7 @@ void loop()
         datosParaEnviar.presenciaEntrada = (tiempoActual - ultimaVezDetectadoEntrada < tiempoEsperaEntrada);
     }
 
-    // 2. LÓGICA DE FIESTA
+    // 3. FIESTA
     int valorLuz = analogRead(LDR_PIN);
     if (valorLuz > umbralLuz)
     {
@@ -102,7 +137,7 @@ void loop()
     }
     datosParaEnviar.fiestaActiva = fiestaActiva;
 
-    // 3. TOCAR MÚSICA (Si la fiesta está activa)
+    // 4. MÚSICA
     if (fiestaActiva)
     {
         if (tiempoActual - tiempoUltimaNota >= duracionNotas[notaActual])
